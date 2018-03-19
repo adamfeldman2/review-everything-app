852ec15c4f6acfb77f4fd4948d802a7e703665d9
Adam Feldman <feldman238@gmail.com>
2018-03-19 10:07:59 -0400

    Update link to portfolio

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -6,7 +6,7 @@ const Footer = () => {
       <div className="wrapper wrapper-footer-content">
         <p>
           Created by:{' '}
-          <a href="http://adamfeldman.io" target="_blank" rel="noopener noreferrer">
+          <a href="https://adamcodes.ca" target="_blank" rel="noopener noreferrer">
             Adam Feldman
           </a>
         </p>
